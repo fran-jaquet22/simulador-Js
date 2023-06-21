5eceeb764d577b9f340d77a0dc01558090ea4908
Fran Jaquet <franciscojaquet29@gmail.com>
2023-06-21 19:29:51 -0300

    Se añadio metodos de busqueda y filtrado

--- style.js
+++ style.js
@@ -35,34 +35,114 @@ function horarios(){
     return
 }
 
-let salirMenu = false
+//class constructora
 
-do{
-let opcionIngresada = parseInt(prompt(`Ingrese la opción deseada
-   1 - Contacto
-   2 - Visita a la fabrica
-   3 - Deja tu CV
-   4 - Obtene nuestras ofertas semanales
-   0 - Salir del menu`))
-   switch(opcionIngresada){
-      case 1:
-         alert("Mail de contacto: fran@gmail.com")
-        break
-      case 2:
-         horarios()
-        break
-      case 3:
-         alert("Envianos tu CV al mail que aparece acontinuacion: trabaja@gmail.com")
-        break   
-      case 4:
-        solicitarMail()
-        break
-      case 0:
-         alert(`Gracias por utilizar nuestra app. Saludos!`)
-         salirMenu = true
-        break   
-      default:
-         alert("Opción no válida, ingrese alguna presente en el menu")
-        break
-   }
-}while(!salirMenu)
+class catalogoA{
+    constructor(id, marca, producto, precio){
+        this.id = id
+        this.marca = marca
+        this.producto = producto
+        this.precio = precio
+    }
+}
+
+//Instalacion de objetos
+
+const item1 = new catalogoA (1, "Castrol", "Aceite", 4000)
+
+const item2 = new catalogoA (2, "Mareno", "Filtro", 1200)
+
+const item3 = new catalogoA (3, "Total", "Aceite", 3500)
+
+const item4 = new catalogoA (4, "Castrol", "Aceite", 2000)
+
+const item5 = new catalogoA (5, "Motul", "Aceite", 2700)
+
+const item6 = new catalogoA (6, "Malhe", "Filtro", 1250)
+
+const item7 = new catalogoA (7, "ACdelco", "Filtro", 1200)
+
+const estanteria = []
+estanteria.push(item1, item2, item3, item4, item5, item6, item7)
+//console.log(estanteria)
+
+function verCatalogo(array){
+    console.log(`Nuestro catalogo es: `)
+    
+    for(let item of array){
+        console.log(item.id, item.marca, item.producto, item.precio)
+    }
+}
+
+//Metodo Busqueda
+function encontrarPor(array){
+    let datoBusqueda = prompt(`Ingrese la marca o producto que desea encontrar`)
+    let busqueda = array.find(
+        (dato) => dato.marca.toLowerCase() == datoBusqueda.toLowerCase() || dato.producto.toLowerCase() == datoBusqueda.toLowerCase() 
+    )
+    if(busqueda == undefined){
+        console.log(`El dato ${datoBusqueda} no esta en nuestro catalogo`)
+    }else{
+        console.log(busqueda)
+    }
+}
+//encontrarPor(estanteria)
+
+//Metodo Filtrado
+function filtrarPorMarcaProducto(array){
+    let marcaBusqueda = prompt(`Ingrese la marca que esta buescando`)
+    let busqueda = array.filter(
+        (dato) => dato.marca.toLowerCase() == marcaBusqueda.toLowerCase() || dato.producto.toLowerCase() == marcaBusqueda.toLowerCase()
+    )
+    if(array.length == 0){
+        console.log(`Para la marca ${marcaBusqueda} no se encontraron coincidencias`)
+    }else{
+        verCatalogo(busqueda)
+    }
+}
+//filtrarPorMarcaProducto(estanteria)
+
+function menu(){
+    
+    let salirMenu = false
+    
+    do{
+    let opcionIngresada = parseInt(prompt(`Ingrese la opción deseada
+       1 - Contacto
+       2 - Visita a la fabrica
+       3 - Ver el catalogo
+       4 - buscar por marca o producto especifico
+       5 - filtra por marca o producto
+       6 - Obtene nuestras ofertas semanales
+       0 - Salir del menu`))
+       switch(opcionIngresada){
+          case 1:
+             alert("Mail de contacto: fran@gmail.com")
+            break
+          case 2:
+             horarios()
+            break
+          case 3:
+             verCatalogo(estanteria)
+            break 
+            case 4:
+            encontrarPor(estanteria)
+            break  
+            case 5:
+            filtrarPorMarcaProducto(estanteria)
+            break
+          case 6:
+            solicitarMail()
+            break
+          case 0:
+             alert(`Gracias por utilizar nuestra app. Saludos!`)
+             salirMenu = true
+            break   
+          default:
+             alert("Opción no válida, ingrese alguna presente en el menu")
+            break
+       }
+    }while(!salirMenu)
+}
+
+menu()
